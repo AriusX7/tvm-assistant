2842a4bb774eed5476debd9bc5476ffce123b8f5
AriusX7 <icyligii@gmail.com>
2020-06-20 19:44:42 +0530

    Use database::initialize_tables in `guild_create`

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ use serenity::{
 };
 use sqlx::PgPool;
 use std::{collections::HashSet, env, sync::Arc};
-use utils::database::obtain_pool;
+use utils::database::{obtain_pool, initialize_tables};
 
 #[macro_use]
 extern crate lazy_static;
@@ -60,80 +60,7 @@ impl EventHandler for Handler {
             return;
         }
 
-        let data_read = ctx.data.read().await;
-        let pool = data_read.get::<ConnectionPool>().unwrap();
-
-        // config table initialization
-        if let Err(why) = sqlx::query!(
-            r#"
-            INSERT INTO config (
-                guild_id,
-                can_change_na,
-                tvmset_lock,
-                signups_on,
-                total_players,
-                total_signups,
-                cycle
-            ) VALUES (
-                $1,
-                true,
-                false,
-                true,
-                12,
-                0,
-                '{ "number": 0, "day": null, "night": null, "votes": null }'
-            ) ON CONFLICT (guild_id) DO NOTHING;
-            "#,
-            guild.id.0 as i64
-        )
-        .execute(pool)
-        .await
-        {
-            error!(
-                "Error initializing config table for guild with ID `{}`: {}",
-                guild.id.0, why
-            );
-        }
-
-        // logging table initialization
-        if let Err(why) = sqlx::query!(
-            "
-            INSERT INTO logging (
-                guild_id
-            ) VALUES (
-                $1
-            ) ON CONFLICT (guild_id) DO NOTHING;
-            ",
-            guild.id.0 as i64
-        )
-        .execute(pool)
-        .await
-        {
-            error!(
-                "Error initializing logging table for guild with ID `{}`: {}",
-                guild.id.0, why
-            );
-        }
-
-        // prefixes table initialization
-        if let Err(why) = sqlx::query!(
-            "
-            INSERT INTO prefixes (
-                guild_id
-            ) VALUES (
-                $1
-            ) ON CONFLICT (guild_id) DO NOTHING;
-            ",
-            guild.id.0 as i64
-        )
-        .execute(pool)
-        .await
-        {
-            error!(
-                "Error initializing prefixes table for guild with ID `{}`: {}",
-                guild.id.0, why
-            );
-        }
+        initialize_tables(&ctx, &guild).await;
     }
 
     async fn message_update(
