943896e16cbd57b1c717dfbd42a994d387209e09
AriusX7 <icyligii@gmail.com>
2020-07-02 22:58:37 +0530

    Add function to get jump url with guild

--- src/utils/message.rs
+++ src/utils/message.rs
@@ -1,4 +1,4 @@
-use serenity::model::prelude::Message;
+use serenity::model::prelude::{Message, GuildId};
 
 pub(crate) fn get_jump_url(msg: &Message) -> String {
     if let Some(guild_id) = msg.guild_id {
@@ -7,3 +7,10 @@ pub(crate) fn get_jump_url(msg: &Message) -> String {
         format!("https://discord.com/channels/@me/{}/{}", msg.channel_id.0, msg.id.0)
     }
 }
+
+/// Messages not received over gateway don't have guild_id and that can
+/// create a malfunctioning link.
+pub(crate) fn get_jump_url_with_guild(msg: &Message, guild_id: &GuildId) -> String {
+    format!("https://discord.com/channels/{}/{}/{}", guild_id.0, msg.channel_id.0, msg.id.0)
+}
+
